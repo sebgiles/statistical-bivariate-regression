85b55b395f2c1009cb2b10a335c2e9ae684de492
sebgiles <mrsebgiles@gmail.com>
2017-09-04 17:18:48 +0200

    confronto 4 modelli vs dati

--- gfr.m
+++ gfr.m
@@ -1,17 +1,40 @@
 function [] = gfr()
+
+    % ======= IMPORT data ============
     % stop warning about names being changed (maybe a bug)
     warning('off','all')
     T = readtable('table1.xlsx');
     warning('on','all')
+    n = height(T);
 
-    y = T.mGFR;
-    n = length(y);
-    cr = (T.CrE + T.CrIDMS) / 2;
-    X = [cr T.Age T.Gender];
+    % ====== CALCULATE AVERAGE measured CR & SORT by it =========
+    T.Cr = (T.CrE + T.CrIDMS) / 2;
+    T = sortrows(T,'Cr');
 
+    % ===== CALCULATE MDRD GFR estimation =====
+    T.MDRD =  186 * T.Cr.^-1.154  ...
+              .* T.Age.^-0.203    ...
+              .* (0.742).^(1-T.Gender);
+
+    % ===== CALCULATE CKD-EPI GFR estimation =====
+    T.CKDEPI =  141 ...
+                * min(T.Cr./(0.7+0.2*T.Age), 1) .^ -(0.329+0.082*T.Gender) ...
+                .* max(T.Cr./(0.7+0.2*T.Age), 1) .^ -1.209 ...
+                .* 0.993 .^T.Age ...
+                .* (1.018-0.018 * T.Gender);
+
+    % ===== CALCULATE Mayo Quadratic GFR estimation =====
+    T.Mayo = exp( 1.911                     ...
+                  +5.249./max(T.Cr, 0.8)     ...
+                  -2.114./max(T.Cr, 0.8).^2  ...
+                  -0.00686*T.Age            ...
+                  -0.205*(1-T.Gender)       );
+
+
+    % ====== PLOT CR vs GFR ======
     figure('position', [80, 80, 400, 720])
     subplot(2,1,1)
-    scatter(T.CrE, y, 7, [1-X(:,3),zeros(n,1),X(:,3)])
+    scatter(T.CrE, T.mGFR, 7, [1-T.Gender,zeros(n,1),T.Gender])
     hold on
     plot(NaN,NaN,'ob'); % dummy set to add second entry to legend, which by
                         % default does not describe conditional colouring
@@ -21,7 +44,7 @@ function [] = gfr()
     ylabel('GFR [mL/min]')
 
     subplot(2,1,2)
-    scatter(T.CrIDMS, y, 7, [1-X(:,3),zeros(n,1),X(:,3)])
+    scatter(T.CrIDMS, T.mGFR, 7, [1-T.Gender,zeros(n,1),T.Gender])
     hold on
     plot(NaN,NaN,'ob'); % dummy like above
     legend('female','male');
@@ -33,6 +56,7 @@ function [] = gfr()
     pause
     close
 
+    % ===== SHOW measurement discrepancies ======
     figure('position', [80, 80, 400, 720])
     subplot(2,1,1)
     scatter(T.CrE, T.CrIDMS)
@@ -42,7 +66,7 @@ function [] = gfr()
     title('Discrepancy between measurement techniques')
 
     subplot(2,1,2)
-    h = histogram( 100*(T.CrIDMS - T.CrE)./cr);
+    h = histogram( 100*(T.CrIDMS - T.CrE)./T.Cr);
     h.NumBins = 20;
     title('Relative difference distribution')
     xlabel('Relative difference [%]')
@@ -57,8 +81,9 @@ function [] = gfr()
     close
     clear h
 
+    % ==== PLOT AGE vs GFR =====
     figure('position', [80, 80, 400, 400])
-    scatter(X(:,2),y, 7, [1-X(:,3),zeros(n,1),X(:,3)])
+    scatter(T.Age,T.mGFR, 7, [1-T.Gender,zeros(n,1),T.Gender])
     title('Influence of age on GFR')
     xlabel('Age')
     ylabel('GFR [mL/min]')
@@ -71,16 +96,18 @@ function [] = gfr()
     pause
     close
 
+    % ===== 3D CR vs AGE vs GFR scatter =====
     figure('position', [80, 80, 600, 600])
-    scatter3(X(:,1),X(:,2),y, 7, [1-X(:,3),zeros(n,1),X(:,3)])
+    scatter3(T.Cr,T.Age,T.mGFR, 7, [1-T.Gender,zeros(n,1),T.Gender])
     hold on
     plot(NaN,NaN,'ob'); % dummy like above
 
-
+    % ===== MDRD model overlay =====
     % preparo vettori per graficare la legge empiriche nota
     m = 20; % numero di punti per asse per il grafico
     [gridCR, gridAge] = ...
-      meshgrid(linspace(min(cr), max(cr),m), linspace(max(T.Age), min(T.Age),m));
+      meshgrid(linspace(min(T.Cr), max(T.Cr),m), ...
+        linspace(max(T.Age), min(T.Age),m));
     % eGFR = 186 x CR^(-1.154) x age^(-0.203) x 0.742^(1-gender)
     mdrdM = 186 * gridCR.^-1.154 .* gridAge.^-0.203;
     mdrdF = 186 * gridCR.^-1.154 .* gridAge.^-0.203 * 0.742;
@@ -105,4 +132,23 @@ function [] = gfr()
     pause
     close
 
+    % ===== compare measurements and estimates =====
+    figure('position', [80, 80, 600, 600])
+    scatter(T.Cr, T.mGFR, 7, [1-T.Gender,zeros(n,1),T.Gender])
+    hold on
+    plot(NaN,NaN,'ob'); % dummy set to add second entry to legend, which by
+                        % default does not describe conditional colouring
+
+    plot(T.Cr, T.MDRD)
+    plot(T.Cr, T.CKDEPI)
+    plot(T.Cr, T.Mayo)
+    plot(T.Cr, T.Schwartz2009)
+
+
+    legend('female','male', 'MDRD', 'CKD-EPI', 'Mayo Quadratic', 'Schwartz2009');
+    xlabel('Creatinine concentration [mg/dL]')
+    ylabel('GFR [mL/min]')
+
+    pause
+    close
 end
