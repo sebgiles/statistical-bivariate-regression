82caf5011d98dd7b486c0086235c61d6fbff39ec
sebgiles <mrsebgiles@gmail.com>
2017-09-08 11:55:02 +0200

    fixed divide by 2 error

--- sbr.m
+++ sbr.m
@@ -8,19 +8,23 @@ function [X,Y] = sbr(Dx,Dy,R,M)
 
   nx = length(Dx);
   ny = length(Dy);
+
   Bx = ceil(20*log10(nx));
   By = ceil(20*log10(ny));
+
   [px,x] = hist(Dx, Bx);
   [py,y] = hist(Dy, By);
+
   px = px'/nx;
   py = (ny*py'+1)/(ny^2+By);
   Px = cumsum(px);
   Py = cumsum(py);
-  x = [min(Dx); x' + (max(Dx)-min(Dx))/Bx];
-  y = [min(Dy); y' + (max(Dy)-min(Dy))/By];
   Px = [0; Px];
   Py = [0; Py];
-  X = linspace(max(Dx), min(Dx), R)';
+  x = [min(Dx); x' + (max(Dx)-min(Dx))/(2*Bx)];
+  y = [min(Dy); y' + (max(Dy)-min(Dy))/(2*By)];
+
+  X = linspace(min(Dx), max(Dx), R)';
   if strcmp(M, 'Increasing')
     Pxi = interp1(x,Px,X);
   else
