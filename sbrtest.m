5e2fe30254c8f104b656de181a51683f65b5db50
sebgiles <mrsebgiles@gmail.com>
2017-09-13 09:14:17 +0200

    checking inversion of pmf is correct

--- sbrtest.m
+++ sbrtest.m
@@ -1,54 +1,61 @@
 function [] = sbrtest()
 
-    % ======= IMPORT data ============
-    % stop warning about names being changed (maybe a bug)
-    warning('off','all')
-    T = readtable('table1.xlsx');
-    warning('on','all')
-    n = height(T);
-    T.Gender = logical(T.Gender);
+  % ======= IMPORT data ============
+  % stop warning about names being changed (maybe a bug)
+  warning('off','all')
+  T = readtable('table1.xlsx');
+  warning('on','all')
+  n = height(T);
+  T.Gender = logical(T.Gender);
 
-    set(0,'DefaultFigureColormap',winter);
+  set(0,'DefaultFigureColormap',winter);
 
-    % ====== CALCULATE AVERAGE measured CR & SORT by it =========
-    T.Cr = (T.CrE + T.CrIDMS) / 2;
-    T = sortrows(T,'Cr');
+  % ====== CALCULATE AVERAGE measured CR & SORT by it =========
+  T.Cr = (T.CrE + T.CrIDMS) / 2;
+  T = sortrows(T,'Cr');
 
-    disp(' ')
-    disp('Showing result of statistical bivariate regression.')
+  disp(' ')
+  disp('Showing result of statistical bivariate regression.')
 
-    figure('position', [80, 80, 600, 600])
-    hold on
-    scatter(T.Cr(~T.Gender), T.mGFR(~T.Gender), [], T.Age(~T.Gender), ...
-      'filled', 'o')
-    scatter(T.Cr(T.Gender), T.mGFR(T.Gender), [], T.Age(T.Gender), ...
-      'filled', '^')
-    h = colorbar;
-    legend('female','male');
-    xlabel('Creatinine concentration [mg/dL]')
-    ylabel('mGFR [mL/min]')
-    title(h, 'age')
+  figure('position', [80, 80, 600, 600])
+  hold on
+  scatter(T.Cr(~T.Gender), T.mGFR(~T.Gender), [], T.Age(~T.Gender), ...
+  'filled', 'o')
+  scatter(T.Cr(T.Gender), T.mGFR(T.Gender), [], T.Age(T.Gender), ...
+  'filled', '^')
+  h = colorbar;
+  legend('female','male');
+  xlabel('Creatinine concentration [mg/dL]')
+  ylabel('mGFR [mL/min]')
+  title(h, 'age')
 
-    [X,Y] = sbr(T.Cr,T.mGFR,100000,'Decreasing');
-    plot(X,Y,'r-','LineWidth',1)
+  [X,Y] = sbr(T.Cr,T.mGFR,10000,'Decreasing');
+  plot(X,Y,'r-','LineWidth',1)
 
-    newY = newsbr(T.Cr,T.mGFR,X);
-    plot(X,newY,'b-','LineWidth',1)
+  newY = newsbr(T.Cr,T.mGFR,X);
+  plot(X,newY,'b-','LineWidth',1)
 
-    [X,fY] = stmd(T.Cr,T.mGFR,100000);
-    plot(X,fY,'g-','LineWidth',1)
+  exnewY = exnewsbr(T.Cr,T.mGFR,X);
+  plot(X,exnewY,'g-','LineWidth',1)
 
+  legend('female','male', 'nans', 'newSBR','exnewsbr');
 
-    legend('female','male', 'SBR', 'newSBR','fiori');
+  pause
+  close
 
-    pause
-    close
+  disp(' ')
+  disp('testing invpmf(pmf(x)) = x for creatinine data')
+  plot(X, invpmf(T.Cr, pmf(T.Cr, X)))
+  grid on
+  pause
+  close
 
-    plot([0:1/100000:1]',invpmf(T.mGFR/180, pmf(T.mGFR/180, [0:1/100000:1]')))
-    grid on
-    hold on
-    plot([0:1/100000:1]',invpmf(T.Cr/2.5, pmf(T.Cr/2.5, [0:1/100000:1]')))
-    pause
-    close
+  disp(' ')
+  disp('testing invpmf(pmf(x)) = x for mGFR data')
+  X = linspace(min(T.mGFR), max(T.mGFR), 10000)';
+  plot(X,invpmf(T.mGFR, pmf(T.mGFR, X)))
+  grid on
+  pause
+  close
 
 end
