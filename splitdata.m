35f16f4b7bc3e3a9a70e74ab3af85c7ef9e121be
sebgiles <mrsebgiles@gmail.com>
2019-02-13 07:29:10 +0100

    split into train and test

--- splitdata.m
+++ splitdata.m
@@ -1,5 +1,7 @@
 function [train, test] = splitdata(T, proportion)
     n = height(T);
     rand_indexes = randperm(n) <= n*proportion;
-    train = T;%(rand_indexes,:);
-    test = T;%(~rand_indexes,:);
+    train = T(rand_indexes,:);
+    test = T(~rand_indexes,:);
+    %train = T; 
+    %test = T;
